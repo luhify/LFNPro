099e996f6d664e74359fc2ad8b4f69018303ed4e
luhify <luhifyishim@outlook.com>
2024-06-09 06:16:23 -0500

    thanksforusing.bat

---
+++ thanksforusing.bat
@@ -0,0 +1,34 @@
+@echo off
+rmdir C:\LFNPro\resourcces >nul 2>&1
+color 6
+echo. 
+echo. 
+echo.
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                            LFNPro has been fully implemented on your system after your restart...
+echo.                            Thanks for using LFNPro  and enjoy that fps and 0 ping you wanted!
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+start https://discord.gg/dvFUyPJ2Be
